117dec3a50494a43e1b4d67c50eab588dd3e167c
Niña Margarette <marga18nins@gmail.com>
2025-03-15 15:48:38 +0800

    continue parser

--- src/parser/Expression.java
+++ src/parser/Expression.java
@@ -3,14 +3,22 @@ package parser;
 import lexer.Token;
 
 public abstract class Expression {
+
+    // < expression >   -> < unaru >
+    // < unary >        -> ( ADD | MINUS) ( < literal > | IDENTIFIER )
     static class Unary extends Expression{
         final Token op;
+        final Expression right;
 
-        public Unary(Token op) {
+        public Unary(Token op, Expression right) {
             this.op = op;
+            this.right = right;
         }
     }
 
+    // < expression >           -> < binary_operation >
+    // < binary_operation >     -> < expression > < operator >  < expression >
+    // < operator >             -> ADD | MINUS | MULT | DIV | MOD | GT | LT | GOE | LOE | EQUAL | NOT_EQUAL
     static class Binary extends Expression{
         final Expression left, right;
         final Token operator;
@@ -22,6 +30,22 @@ public abstract class Expression {
         }
     }
 
+    // < expression >           -> < logic_operation >
+    // < logic_operation >      -> < expression > < logic_operators >  < expression >
+    // < logic_operators >      -> AND | OR | NOT
+    static class Logic extends Expression{
+        final Expression left, right;
+        final Token logic_op;
+
+        public Logic(Expression left, Token logic_op, Expression right) {
+            this.left = left;
+            this.right = right;
+            this.logic_op = logic_op;
+        }
+    }
+
+    // < expression >   -> < group >
+    // < group >        -> OPEN_P < expression > CLOSE_P
     static class Group extends Expression{
         final Expression expression;
 
@@ -30,11 +54,36 @@ public abstract class Expression {
         }
     }
 
+    // < expression >   -> < literal >
+    // < literal >        -> INTEGER | DOUBLE | STRING | CHARACTERS | BOOLEAN
     static class Literal extends Expression{
         final Token literalType;
+        final Object value;
 
-        public Literal(Token literalType) {
+        public Literal(Token literalType, Object value) {
             this.literalType = literalType;
+            this.value = value;
+        }
+    }
+
+    // < expression >   -> < assign >
+    // < assign >    -> IDENTIFIER ASSIGN_OP ( < assign > | < expression >)
+    static class Assign extends Expression{
+        final Token name;
+        final Expression expression;    // or value
+
+        public Assign(Token name, Expression expression) {
+            this.name = name;
+            this.expression = expression;
+        }
+    }
+
+    // < expression > -> IDENTIFIER
+    static class Variable extends Expression{
+        final Token name;
+
+        public Variable(Token name) {
+            this.name = name;
         }
     }
 
--- src/parser/Statement.java
+++ src/parser/Statement.java
@@ -16,25 +16,31 @@ public abstract class Statement {
     }
 
     // < statement >            ->  < output_statement >
-    // < output_statement >     ->  OUTPUT COLON <  >
+    // < output_statement >     ->  OUTPUT COLON < expression >
     static class Output extends Statement{
-        public Output() {
+        final Expression expression;
+        public Output(Expression expression) {
+            this.expression = expression;
         }
     }
 
     // < statement >            ->  < input_statement >
     // < input_statement >     ->  INPUT COLON IDENTIFIER (COMMA IDENTIFIER)*
     static class Input extends Statement{
-        public Input() {
+        final Expression variables;
+        public Input (Expression variables) {
+            this.variables = variables;
         }
     }
 
     static class VarDeclaration extends Statement{
         final Token type;
+        final Expression initialization;
 //        final Variable var;
 
-        VarDeclaration(Token type) {
+        VarDeclaration(Token type, Token name, Expression initialization) {
             this.type = type;
+            this.initialization = initialization;
 //            this.var = var;
         }
     }
