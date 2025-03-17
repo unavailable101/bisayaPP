35349e2a2c90cba4439236e0bd5af883df72c415
Niña Margarette <marga18nins@gmail.com>
2025-03-17 23:32:49 +0800

    Parser - trust the process

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -66,18 +66,17 @@ public class Parser {
 
     private Statement varDeclare(List<Token> tokens){
         Token dataType = consume(currToken(tokens), TokenType.DATA_TYPE, "Walay Data Type");
-        Expression initialzer = null;
-
+        Expression initialzer = expression(tokens);
         return new Statement.VarDeclaration(dataType, initialzer);
     }
 
     private Statement outputStatement(List<Token> tokens){
-        if (currToken(tokens).getType() == TokenType.COLON)  return new Statement.Output(expression(tokens));
+        if (currToken(tokens).getType() == TokenType.COLON)  return new Statement.Output(expression(tokens));       // sure sd ka na expression ang naa dire? omg nimo giiirl
         throw new RuntimeException("Missing ':' after " + tokens.getFirst());
     }
 
     private Statement inputStatement(List<Token> tokens){
-        if (currToken(tokens).getType() == TokenType.COLON)  return new Statement.Input(expression(tokens));
+        if (currToken(tokens).getType() == TokenType.COLON)  return new Statement.Input(expression(tokens));        //sure ka na expression ni dire girl?
         throw new RuntimeException("Missing ':' after " + tokens.getFirst());
     }
 
@@ -94,59 +93,177 @@ public class Parser {
     //                         < group_expression >    |
     //                         < assign >
     private Expression expression(List<Token> tokens){
-        // literals
+        return assignment(tokens);
+    }
+
+    private Expression assignment(List<Token> tokens){
+        Expression expr = logicalOr(tokens);
+
         if (
-                currToken(tokens).getType() == TokenType.INTEGER    ||
-                currToken(tokens).getType() == TokenType.DOUBLE     ||
-                currToken(tokens).getType() == TokenType.CHARACTERS ||
-                currToken(tokens).getType() == TokenType.STRING     ||
-                currToken(tokens).getType() == TokenType.BOOLEAN
-        ) return new Expression.Literal(currToken(tokens));
-
-
-        // identifiers/variables
-        if (currToken(tokens).getType() == TokenType.IDENTIFIER){
-            if (indx < tokens.size()){
-                nextToken(tokens);
-                if (currToken(tokens).getType() == TokenType.ASS_OP) return new Expression.Assign(prevToken(tokens), expression(tokens));
-                if (
-                        currToken(tokens).getType() == TokenType.ARITH_GT       ||
-                        currToken(tokens).getType() == TokenType.ARITH_LT       ||
-                        currToken(tokens).getType() == TokenType.ARITH_GOE      ||
-                        currToken(tokens).getType() == TokenType.ARITH_LOE      ||
-                        currToken(tokens).getType() == TokenType.ARITH_EQUAL    ||
-                        currToken(tokens).getType() == TokenType.ARITH_NOT_EQUAL
-                ) return new Expression.Compare(expression(tokens), currToken(tokens), expression(tokens));
-                if (
-                        currToken(tokens).getType() == TokenType.LOG_OR     ||
-                        currToken(tokens).getType() == TokenType.LOG_NOT    ||
-                        currToken(tokens).getType() == TokenType.LOG_AND
-                ) return new Expression.Logic(expression(tokens), currToken(tokens), expression(tokens));
-
-                if (
-                        currToken(tokens).getType() == TokenType.ARITH_ADD      ||
-                        currToken(tokens).getType() == TokenType.ARITH_MINUS    ||
-                        currToken(tokens).getType() == TokenType.ARITH_DIV      ||
-                        currToken(tokens).getType() == TokenType.ARITH_MULT     ||
-                        currToken(tokens).getType() == TokenType.ARITH_MOD
-                ) return new Expression.Binary(expression(tokens), currToken(tokens), expression(tokens));
-
-            }
-            return new Expression.Variable(currToken(tokens));
+                currToken(tokens).getType() == TokenType.IDENTIFIER &&
+                nextToken(tokens).getType() == TokenType.ASS_OP
+        ){
+            Token var = prevToken(tokens);
+            nextToken(tokens);
+            return new Expression.Assign(var, assignment(tokens));
         }
 
-        if (
+        return expr;
+    }
+
+    private Expression logicalOr(List<Token> tokens){
+        Expression expr = logicalAnd(tokens);
+
+        while (
+                currToken(tokens).getType() == TokenType.LOG_OR
+        ){
+            Token op = currToken(tokens);
+            nextToken(tokens);
+            Expression right = logicalAnd(tokens);
+            nextToken(tokens);
+            expr = new Expression.Binary(expr, op, right);
+        }
+
+        return expr;
+    }
+
+    private Expression logicalAnd(List<Token> tokens){
+        Expression expr = equality(tokens);
+
+        while (
+                currToken(tokens).getType() == TokenType.LOG_AND
+        ){
+            Token op = currToken(tokens);
+            nextToken(tokens);
+            Expression right = equality(tokens);
+            nextToken(tokens);
+            expr = new Expression.Logic(expr, op, right);
+        }
+
+        return expr;
+    }
+
+    private Expression equality(List<Token> tokens){
+        Expression expr = comparison(tokens);
+
+        while (
+                currToken(tokens).getType() == TokenType.ARITH_EQUAL ||
+                currToken(tokens).getType() == TokenType.ARITH_NOT_EQUAL
+        ){
+            Token op = currToken(tokens);
+            nextToken(tokens);
+            Expression right = comparison(tokens);
+            nextToken(tokens);
+            expr = new Expression.Compare(expr, op, right);
+        }
+
+        return expr;
+    }
+
+    private Expression comparison (List<Token> tokens){
+        Expression expr = term(tokens);
+
+        while (
+                currToken(tokens).getType() == TokenType.ARITH_GT ||
+                currToken(tokens).getType() == TokenType.ARITH_LT ||
+                currToken(tokens).getType() == TokenType.ARITH_GOE ||
+                currToken(tokens).getType() == TokenType.ARITH_LOE
+        ){
+            Token op = currToken(tokens);
+            nextToken(tokens);
+            Expression right = term(tokens);
+            nextToken(tokens);
+            expr = new Expression.Compare(expr, op, right);
+        }
+
+        return expr;
+    }
+
+    private Expression term (List<Token> tokens){
+        Expression expr = factor(tokens);
+
+        while (
                 currToken(tokens).getType() == TokenType.ARITH_ADD ||
                 currToken(tokens).getType() == TokenType.ARITH_MINUS
         ){
-            return new Expression.Unary(currToken(tokens), expression(tokens));
+            Token op = currToken(tokens);
+            nextToken(tokens);
+            Expression right = factor(tokens);
+            nextToken(tokens);
+            expr = new Expression.Binary(expr, op, right);
         }
 
-        return null;
+        return expr;
+    }
+
+    private Expression factor(List<Token> tokens){
+        Expression expr = unary(tokens);
+
+        while (
+                currToken(tokens).getType() == TokenType.ARITH_MULT ||
+                currToken(tokens).getType() == TokenType.ARITH_DIV ||
+                currToken(tokens).getType() == TokenType.ARITH_MOD
+        ){
+            Token op = currToken(tokens);
+            nextToken(tokens);
+            Expression right = unary(tokens);
+            nextToken(tokens);
+            expr = new Expression.Binary(expr, op, right);
+        }
+
+        return expr;
+    }
+
+    private Expression unary(List<Token> tokens){
+        if (
+                currToken(tokens).getType() == TokenType.ARITH_ADD ||
+                currToken(tokens).getType() == TokenType.ARITH_MINUS ||
+                currToken(tokens).getType() == TokenType.LOG_AND
+        ){
+            Token op = currToken(tokens);
+            nextToken(tokens);
+            Expression right = unary(tokens);
+            return new Expression.Unary(op, right);
+        }
+
+        return primary(tokens);
+    }
+
+    private Expression primary(List<Token> tokens){
+
+        Expression expr;
+
+        switch (currToken(tokens).getType()){
+            //literals
+            case TokenType.INTEGER:
+            case TokenType.DOUBLE:
+            case TokenType.BOOLEAN:
+            case TokenType.CHARACTERS:
+            case TokenType.STRING:
+                expr = new Expression.Literal(currToken(tokens));
+                nextToken(tokens);
+                return expr;
+
+            //variables
+            case TokenType.IDENTIFIER:
+                expr = new Expression.Variable(currToken(tokens));
+                nextToken(tokens);
+                return expr;
+
+             //if group sha
+            case TokenType.ARITH_OPEN_P:
+                nextToken(tokens);
+                expr = expression(tokens);
+                if (currToken(tokens).getType() != TokenType.ARITH_CLOSE_P) throw new RuntimeException("Expected ')' after expression.");
+                nextToken(tokens);
+                return new Expression.Group(expr);
+        }
+
+        throw new RuntimeException("Expected expression.");
     }
 
     private Token nextToken (List<Token> tokens){
-        return indx < tokens.size() ? tokens.get(indx+1) : tokens.get(tokens.size()-1);
+        return indx < tokens.size() ? tokens.get(indx++) : tokens.get(tokens.size()-1);
     }
 
     private Token currToken (List<Token> tokens){
