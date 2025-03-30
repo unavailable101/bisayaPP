655d25f6fd3944d400e93889acfa2ea80e88f3c9
Niña Margarette <marga18nins@gmail.com>
2025-03-30 14:37:55 +0800

    ambot Interpreter

--- src/interpreter/Interpreter.java
+++ src/interpreter/Interpreter.java
@@ -1,12 +1,10 @@
 package interpreter;
 
-import lexer.TokenType;
 import parser.Expression;
 import parser.Statement;
 
 import java.util.List;
 
-import static lexer.TokenType.*;
 
 public class Interpreter implements Expression.Visitor<Object>, Statement.Visitor<Void> {
 
@@ -14,17 +12,19 @@ public class Interpreter implements Expression.Visitor<Object>, Statement.Visito
         // dire sha mag kuha(?) sa mga ano, mga statements then iya i pasa to idk
         // iterate bha daaaaaaa kapoy
         // mag read sha each statements then i pasa to another funtcion then gauuur
+
     }
 
     @Override
     public Object visitUnary(Expression.Unary expression) {
-        Object right = expression.right;
+        Object right = evaluate(expression.right);
 
         switch (expression.op.getType()){
             case ARITH_MINUS:
-                return -(Double)right;
+                ifNumOperand(right);
+                return -(double)right;
             case LOG_NOT:
-                return null;
+                return !isTrue(right);
         }
 
         return null;
@@ -37,15 +37,22 @@ public class Interpreter implements Expression.Visitor<Object>, Statement.Visito
 
         switch (expression.operator.getType()){
             case ARITH_ADD :
-                return (Double)left + (Double)right;
+                ifNumOperands(left, right);
+                return (double)left + (double)right;
             case ARITH_MINUS :
-                return (Double)left - (Double)right;
+                ifNumOperands(left, right);
+                return (double)left - (double)right;
             case ARITH_MULT :
-                return (Double)left * (Double)right;
+                ifNumOperands(left, right);
+                return (double)left * (double)right;
             case ARITH_DIV :
-                return (Double)left / (Double)right;
+                ifNumOperands(left, right);
+                return (double)left / (double)right;
             case ARITH_MOD :
-                return (Double)left % (Double)right;
+                ifNumOperands(left, right);
+                return (double)left % (double)right;
+            case CONCAT:
+                return left + (String)right;
 
         }
         return null;
@@ -75,17 +82,18 @@ public class Interpreter implements Expression.Visitor<Object>, Statement.Visito
 
     @Override
     public Object visitLiteral(Expression.Literal expression) {
-        return null;
+        return expression.literal;
     }
 
     @Override
     public Object visitAssign(Expression.Assign expression) {
+        Object expr = evaluate(expression.expression);
         return null;
     }
 
     @Override
     public Object visitVariable(Expression.Variable expression) {
-        return null;
+        return expression.name;
     }
 
     @Override
@@ -95,17 +103,17 @@ public class Interpreter implements Expression.Visitor<Object>, Statement.Visito
 
         switch (expression.op.getType()){
             case ARITH_EQUAL :
-                return left.equals(right);
+                return isEqual(left, right);
             case ARITH_NOT_EQUAL :
-                return !left.equals(right); // ambot
+                return !isEqual(left, right);
             case ARITH_GT :
-                return (Double)left > (Double)right;
+                return (double)left > (double)right;
             case ARITH_LT :
-                return (Double)left < (Double)right;
+                return (double)left < (double)right;
             case ARITH_LOE :
-                return (Double)left <= (Double)right;
+                return (double)left <= (double)right;
             case ARITH_GOE :
-                return (Double)left >= (Double)right;
+                return (double)left >= (double)right;
         }
 
         return null;
@@ -133,6 +141,27 @@ public class Interpreter implements Expression.Visitor<Object>, Statement.Visito
 
     private Object evaluate (Expression expr){
         return expr.accept(this);
+    }
+
+    private boolean isTrue(Object o){
+        return o == null ? false :
+                o instanceof Boolean ? (boolean)o : true;
+    }
+
+    private boolean isEqual(Object a, Object b){
+        return a == null && b == null ? true :
+                a == null || b == null ? false : a.equals(b);
+    }
+
+    private void ifNumOperand (Object o){
+        if (o instanceof Number) return;
+        throw new RuntimeException("Dili ni sha number");
+    }
+
+    private void ifNumOperands (Object left, Object right){
+        if (left instanceof Number &&
+            right instanceof Number) return;
+        throw new RuntimeException("Dili ni sha number");
     }
 
 }
